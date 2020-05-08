a4d4af9abe1d1c028051394a09ccb420707fe464
Mahmoud Elsokary <mahmoudshedid@gmail.com>
2020-05-08 11:25:08 +0200

    init

---
+++ backend/src/main/java/com/shedid/game/TreasureHunterGameApplication.java
@@ -0,0 +1,13 @@
+package com.shedid.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureHunterGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasureHunterGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shedid/game/TreasureHunterGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shedid.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasureHunterGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
